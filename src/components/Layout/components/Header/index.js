9647ef83ea24f3bb31b421ad84e8ee634d53a58e
NguyenDucAnh2610 <97513336+NguyenDucAnh2610@users.noreply.github.com>
2022-05-12 18:14:55 +0700

    12/5-6:14

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,56 @@
+import { useState, useEffect } from 'react';
+import { CloseCircleFilled, Loading3QuartersOutlined, SearchOutlined } from '@ant-design/icons';
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+
+import AcountItem from '@/components/AcountItem';
+import { wrapper as PopperWrapper } from '@/components/Popper';
+import styles from './Header.module.scss';
+import images from '@/assets/images';
+
+const cx = classNames.bind(styles);
+console.log('🚀 ~ file: index.js ~ line 12 ~ Header ~ images.logo', images.logo);
 function Header() {
-    return <h2>header</h2>;
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([]);
+        }, 0);
+    }, []);
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AcountItem />
+                                <AcountItem />
+                                <AcountItem />
+                                <AcountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search accounts and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <CloseCircleFilled />
+                        </button>
+                        <Loading3QuartersOutlined className={cx('loading')} />
+                        <button className={cx('search-button')}>
+                            <SearchOutlined />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
